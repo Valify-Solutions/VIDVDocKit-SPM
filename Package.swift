9dddd12357d753f92bb96042b3a8ced580d2d4ea
Sayed Obaid <sayed@valify.me>
2026-01-14 23:51:37 +0200

    Update Package.swift for version 1.4.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VIDVDocKit",
-            url: "https://valify-public-sdks.s3.eu-central-1.amazonaws.com/VIDVDocKit/1.3.1/VIDVDocKit.zip",
-            checksum: "5d1ac8d7adf5f81ad6ef3b49d837396672949ee62d080234c6521b615a7e74e9"
+            url: "https://valify-public-sdks.s3.eu-central-1.amazonaws.com/VIDVDocKit/1.4.2/VIDVDocKit.zip",
+            checksum: "438a93e79b5337b283bbb655ed945fc9a090c01bdbeb13fb6996c8a03894f331"
         )
     ]
 )
