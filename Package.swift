3d6af24f5468397e7cbff6f096da226676f98421
Ahmed Attalla <devahmedattalla@gmail.com>
2026-05-13 17:11:49 +0300

    Update Package.swift for version 1.5.5

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VIDVDocKit",
-            url: "https://valify-public-sdks.s3.eu-central-1.amazonaws.com/VIDVDocKit/1.5.4/VIDVDocKit.zip",
-            checksum: "9440fbda3444b7be981e220702da07f08677dc0b87662015ed81c9d413c0d3fd"
+            url: "https://valify-public-sdks.s3.eu-central-1.amazonaws.com/VIDVDocKit/1.5.5/VIDVDocKit.zip",
+            checksum: "9eee0a45658e1f8985f14743cf90b23d028f29e44c388d9e0159b6e6ae197ed2"
         )
     ]
 )
